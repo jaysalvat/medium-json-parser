9841129e4836650c6304cd5538a6e8127ed338ca
Jay Salvat <jay@jaysalvat.com>
2017-01-10 21:32:42 +0100

    Better responsive CSS

--- example/index.php
+++ example/index.php
@@ -19,6 +19,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Medium Parser</title>
     <link rel="stylesheet" href="styles.css">
 </head>
